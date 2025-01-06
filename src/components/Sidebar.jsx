d9ca6afcfec2a16f2cc0c9a403e8db637179d5a1
Berkcan Gümüşışık <75336900+berkcangumusisik@users.noreply.github.com>
2025-01-07 00:39:36 +0300

    Update README.md to provide project details and setup instructions for React with Vite

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,12 @@
+export default function Sidebar() {
+    return (
+        <aside className="w-1/3 px-8 py-16 bg-gray-100 rounded-lg text-stone-500">
+          <h2 className="text-2xl font-bold">Project Name</h2>
+          <div>
+            <button className="bg-stone-500 text-white px-4 py-2 rounded-lg">
+              Add Project
+            </button>
+          </div>
+        </aside>
+    )
+}
